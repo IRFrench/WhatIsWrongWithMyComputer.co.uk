7a72a04d69408e781f409559407fefd34859a712
IRFrench <isaacrf@hotmail.co.uk>
2022-08-03 20:05:22 +0100

    Fix small changes for the blogs page
    - Change the code comment colours

--- js/scrollToBlog.js
+++ js/scrollToBlog.js
@@ -1,6 +1,6 @@
 function BlogLink() {
     console.log(window.location.href)
-    if (window.location.href == "http://127.0.0.1:4000/" | window.location.href == "https://whatiswrongwithmycomputer.co.uk/") {
+    if (window.location.href == "https://www.whatiswrongwithmycomputer.co.uk/" | window.location.href == "https://whatiswrongwithmycomputer.co.uk/") {
         document.getElementById("content").scrollIntoView({ behavior: "smooth", block: "start" })
     } else {
         window.location.href = "/"
